de0013c84efe2b9bd8359d8cef1b358f6e195730
Divyeshs2303 <divyeshsolanki2303@gmail.com>
2025-01-03 11:19:56 +0530

    80% done

--- src/Education.jsx
+++ src/Education.jsx
@@ -1,13 +1,16 @@
-import React, { useEffect } from "react";
-import { gsap } from "gsap";
+import React, { useEffect, useRef } from "react";
 import data from "./assets/data.json";
 import { CiLocationOn } from "react-icons/ci";
 import { IoMdTime } from "react-icons/io";
-import { useGSAP } from "@gsap/react";
 
+console.log("🚀 ~ data:", data);
+console.log("🚀 ~ data:", data?.data);
+console.log("🚀 ~ data:", data?.projects);
 const Education = () => {
+  const containerRef = useRef(null);
+
   return (
-    <div className="container pages4">
+    <div className="container pages4" ref={containerRef}>
       {data?.data?.map((ele, index) => (
         <div className="p-2 card-body" key={index}>
           <h3 className="p-2 degree-title">{ele?.degree}</h3>
